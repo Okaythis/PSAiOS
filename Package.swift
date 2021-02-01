017375ff54a0022cad27c968596d8c8b03ca71e4
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-02-01 13:51:08 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,12 +7,12 @@ let package = Package(name: "OkaySdkLib",
                       targets: [.target(name: "OkaySdkLib"),
                                 .binaryTarget(name: "PSACommon",
                                               url: "https://github.com/Okaythis/PSAiOS/raw/718628415aa7bbc72e4c5daa09aa212e3bd131d4/PSACommon.xcframework.zip",
-                                              checksum: "bfabff6827a014fa34f6496f59b06bea0480f163bc41fc5145fdc9defce5436c"),
+                                              checksum: "38b664c34575b8f8724eb6ee3e4e74d24fdcb64690c56ab795dec59a44a2182d"),
                                 .binaryTarget(name: "DefaultPsaUi",
                                               url: "https://github.com/Okaythis/PSAiOS/raw/e141d8f87b0c3f7de30b9ac08db04573f0511ecb/DefaultPsaUi.xcframework.zip",
-                                              checksum: "1b95bc6a3415b5905d290a56e036bae426c6c75bd4dcc2cd672e09bb5b3f7ab0"),
+                                              checksum: "a1ac181d7649a8d5a9b260d483f15804192d852250e729f4056b0cd49c2ca07a"),
                                 .binaryTarget(name: "PSA",
                                               url: "https://github.com/Okaythis/PSAiOS/raw/765a51a19d98bc95af1c389085eb0ead089dc80d/PSA.xcframework.zip",
-                                              checksum: "fd0b6f916f43e51136096787e79b50c701ce9cd1082b17b86563108002d356e1")
+                                              checksum: "ce78e3b31df048d52500f7d86193e857728b7b20dffc2d3884053ba895bf0bc8")
                                ]
                      )
