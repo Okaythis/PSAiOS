73c53287ce13a2b0f7ffb5c0e2a925065a6bbbaf
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-02-01 13:46:42 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,18 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let package = Package(name: "OkaySDK",
+let package = Package(name: "OkaySdkLib",
                       platforms: [.iOS(.v10)],
-                      products: [.library(name: "OkaySDK", targets: ["PSACommon", "ePaymentsUI", "PSA"])],
-                      targets: [.target(name: "OkaySDK"),
+                      products: [.library(name: "OkaySdkLib", targets: ["PSACommon", "DefaultPsaUi", "PSA"])],
+                      targets: [.target(name: "OkaySdkLib"),
                                 .binaryTarget(name: "PSACommon",
-                                              url: "https://github.com/Okaythis/PSACommonIOS/raw/48c8fcb3dd827c96c773a3b041e91b90ac16bf28/PSACommon.xcframework.zip",
+                                              url: "https://github.com/Okaythis/PSAiOS/raw/718628415aa7bbc72e4c5daa09aa212e3bd131d4/PSACommon.xcframework.zip",
                                               checksum: "bfabff6827a014fa34f6496f59b06bea0480f163bc41fc5145fdc9defce5436c"),
-                                .binaryTarget(name: "ePaymentsUI",
-                                              url: "https://github.com/Okaythis/ePaymentsiOS/raw/e3229824bbeddefed9004b1307065d30bcc4af72/ePaymentsUI.xcframework.zip",
+                                .binaryTarget(name: "DefaultPsaUi",
+                                              url: "https://github.com/Okaythis/PSAiOS/raw/e141d8f87b0c3f7de30b9ac08db04573f0511ecb/DefaultPsaUi.xcframework.zip",
                                               checksum: "1b95bc6a3415b5905d290a56e036bae426c6c75bd4dcc2cd672e09bb5b3f7ab0"),
                                 .binaryTarget(name: "PSA",
-                                              url: "https://github.com/Okaythis/ePaymentsiOS/raw/e3229824bbeddefed9004b1307065d30bcc4af72/PSA.xcframework.zip",
+                                              url: "https://github.com/Okaythis/PSAiOS/raw/765a51a19d98bc95af1c389085eb0ead089dc80d/PSA.xcframework.zip",
                                               checksum: "fd0b6f916f43e51136096787e79b50c701ce9cd1082b17b86563108002d356e1")
                                ]
                      )
