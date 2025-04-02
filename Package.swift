23077935c8d024860ce8eee09641e81175482775
Satheesh Kannan <satheesh@okaythis.com>
2025-04-02 22:26:30 +0530

    chore: update package.swift file for PSASDK v1.3.2

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
             targets: ["PSACommon", "DefaultPsaUi", "PSA", "Watermarker", "FccAbstractCore"]),
     ],
     targets: [
-        .target(name: "OkaySDK"),
         .binaryTarget(name: "PSACommon",
                       url: "https://github.com/Okaythis/PSA_SPM/raw/decf50aaa44883fb39392d3cde677ccca37a9242/PSACommon.xcframework.zip",
                       checksum: "5fdf6d232e75517907f5cd936834c2ad30420e366fc3d753aac108d63882f3ec"),
@@ -22,12 +21,12 @@ let package = Package(
                       url: "https://github.com/Okaythis/PSA_SPM/raw/decf50aaa44883fb39392d3cde677ccca37a9242/DefaultPsaUi.xcframework.zip",
                       checksum: "45b562b75f3b9210ee0088e51c93f9259aef535a39e439ccc71321edad168d10"),
         .binaryTarget(name: "PSA",
-                      url: "https://github.com/Okaythis/PSA_SPM/raw/bd5f6ed1cfe2e7621d287f9114b0a236fa2695e4/PSA.xcframework.zip",
-                      checksum: "a6de53334ec43bebcea5ba74a06c497d4fe88ff7e4f7df56d7aedfd084a5f2de"),
+                      url: "https://github.com/Okaythis/PSA_SPM/raw/1d35cebec08bc6457047302e11e2656067c10d76/PSA.xcframework.zip",
+                      checksum: "1ba74552aa44ca6f9dba5b506f60970d11c73d12354f67ef221958b11848f67e"),
         .binaryTarget(name: "Watermarker",
                       url: "https://github.com/Okaythis/PSA_SPM/raw/decf50aaa44883fb39392d3cde677ccca37a9242/Watermarker.xcframework.zip",
                       checksum: "bf515afbe5e8ee5c510c6b591593db65cfc3f3073c712817100022a2169d06ce"),
     ]
 )
 
-//OkaySDK - v1.3.0
+//OkaySDK - v1.3.2
